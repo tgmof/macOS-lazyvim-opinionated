43954a98cb23b209c98487a2cb0539a8d267f6c7
Tony <31918716+tgmof@users.noreply.github.com>
2026-08-09 22:31:39 +0200

    Fix Alacritty config at setup

--- neovim_for_macOS.sh
+++ neovim_for_macOS.sh
@@ -7,10 +7,15 @@ fi
 
 if [ -d /Applications/Alacritty.app ]; then
   echo "Alacritty is installed"
+  echo "You MAY want to run the following lines to use the fonts installed by this script and default alacritty options:"
+  echo "mkdir -p ~/.config/alacritty/"
+  echo "cp ./alacritty.toml ~/.config/alacritty/alacritty.toml"
 else
   curl -sLO https://github.com/alacritty/alacritty/releases/download/v0.17.0/Alacritty-v0.17.0.dmg
   hdiutil attach hdiutil attach Alacritty-v0.17.0.dmg
   sudo cp -R /Volumes/Alacritty/Alacritty.app /Applications/
+  mkdir -p ~/.config/alacritty/
+  cp ./alacritty.toml ~/.config/alacritty/alacritty.toml
 fi
 # Make Neovim an app-like bundle so that you can start neovim to edit a file from Finder (https://www.reddit.com/r/Ghostty/comments/1hsvjtg/comment/m61htlo/?context=3&share_id=mN8755Rz7x_1gHHC9aVISl)
 sudo cp -R ./Neovim.app /Applications/
